a34b7be1180d729ff598f06bd4bba467e042fffe
Master-Bw3 <bw1jochman@gmail.com>
2025-02-27 21:06:58 -0600

    deck show and hid

--- uiuatro.lua
+++ uiuatro.lua
@@ -46,13 +46,43 @@ mod_dir = '' .. SMODS.current_mod.path
 
 if not UIUATRO then UIUATRO = {} end
 
-
-UIUATRO.STATES = {
-    SELECTING_STACK_HAND = 42,
-}
+UIUATRO.stack_hand = nil;
+UIUATRO.stack_deck = nil;
 
 
-UIUATRO.secondHand = nil;
+
+function UIUATRO:hide_stack_hand()
+    self.stack_hand.states.visible = false
+
+    
+end
+
+function UIUATRO:show_stack_hand()
+    self.stack_hand.states.visible = true
+end
+
+local game_state = -1;
+local on_game_state_change
+on_game_state_change = Event({
+    func = function()
+        if G.STATE == game_state then
+            return false
+        end
+
+        game_state = G.STATE;
+        G.E_MANAGER:add_event(on_game_state_change)
+
+
+        if (G.STATE == G.STATES.SELECTING_HAND) then
+            UIUATRO:show_stack_hand()
+        else
+            UIUATRO:hide_stack_hand()
+        end
+
+        return true
+    end,
+    blocking = false
+})
 
 function UIUATRO:start_up()
 
@@ -61,36 +91,39 @@ end
 function UIUATRO:set_up_ui()
     print("setting up uiuatro ui")
 
-    self.secondHand = CardArea(
+    self.stack_hand = CardArea(
         0, 0,
-        6*G.CARD_W, 0.95*G.CARD_H,
+        6 * G.CARD_W, 0.95 * G.CARD_H,
         { card_limit = G.GAME.starting_params.hand_size, type = 'hand' }
     )
 
+    self.stack_deck = CardArea(
+        0, 0,
+        G.CARD_W * 1.1, 0.95 * G.CARD_H,
+        { card_limit = 52, type = 'deck' })
+
+    G.E_MANAGER:add_event(on_game_state_change)
 end
 
 function UIUATRO:set_screen_positions()
     print("setting positions")
 
-    self.secondHand.T.x = G.TILE_W - self.secondHand.T.w - 2.85
-    self.secondHand.T.y = G.TILE_H - self.secondHand.T.h - 4.8
-    self.secondHand:hard_set_VT()
+    self.stack_hand.T.x = G.TILE_W - self.stack_hand.T.w - 2.85
+    self.stack_hand.T.y = G.TILE_H - self.stack_hand.T.h - 5.5
+    self.stack_hand:hard_set_VT()
 
+    self.stack_deck.T.x = G.TILE_W - self.stack_deck.T.w - 0.5
+    self.stack_deck.T.y = G.TILE_H - self.stack_deck.T.h - 4
+    self.stack_deck:hard_set_VT()
 end
-
 
 function doThing(e)
     print("i pressed")
-    if (G.STATE == G.STATES.SELECTING_HAND) then 
-        draw_card(G.deck, UIUATRO.secondHand, 50, 'up', true)
+    if (G.STATE == G.STATES.SELECTING_HAND) then
+        draw_card(G.hand, UIUATRO.stack_deck, 1, 'up', true)
+
+        draw_card(UIUATRO.stack_deck, UIUATRO.stack_hand, 1, 'up', true)
     end
 end
 
-SMODS.Keybind({key_pressed = "i", action = doThing})
-
-
-
-
-
-
-
+SMODS.Keybind({ key_pressed = "i", action = doThing })
